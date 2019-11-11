e32aea12999cf9a7e51e120fae93b2a2abca1e24
ajay sanu <ajaysanu4@gmail.com>
2019-11-11 10:22:37 +0530

    Added new line in welcome page

--- Angular-GettingStarted-master/APM-Final/src/app/home/welcome.component.ts
+++ Angular-GettingStarted-master/APM-Final/src/app/home/welcome.component.ts
@@ -3,9 +3,13 @@ import { Component } from "@angular/core";
 @Component({
   templateUrl: "./welcome.component.html"
 })
+
+
 export class WelcomeComponent {
   pageTitle: string;
+ 
   
+
   constructor() {
     this.pageTitle = "Welcome";
   }
